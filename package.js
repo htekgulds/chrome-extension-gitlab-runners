44fc6d8ed087c8380eef1ab55b527d21a1acc07e
Hasan TEKGÜL <hasan.tekgul@hmb.gov.tr>
2023-12-08 11:39:40 +0300

    firefox extension

--- package.js
+++ package.js
@@ -2,26 +2,35 @@ const fs = require('fs-extra')
 const targz = require('targz')
 const pkgJson = require('./package.json')
 
-// Make directory
-fs.removeSync('package')
-fs.mkdirSync('package')
-fs.copySync('extension', 'package/chrome-extension-get-runners')
+function copyFiles (env) {
+  // Make directories
+  fs.copySync('extensions/common', `packages/${env}/${env}-extension-gitlab-runners`)
+  fs.copySync(`extensions/${env}`, `packages/${env}/${env}-extension-gitlab-runners`)
 
-// Prepare manifest.json
-const manifest = fs.readFileSync('extension/manifest.json', { encoding: 'utf-8' })
-const manifestWithVersion = manifest.replace('dev', pkgJson.version)
-fs.writeFileSync('package/chrome-extension-get-runners/manifest.json', manifestWithVersion)
+  // Prepare manifest.json
+  const manifest = fs.readFileSync(`extensions/${env}/manifest.json`, { encoding: 'utf-8' })
+  const manifestWithVersion = manifest.replace('dev', pkgJson.version)
+  fs.writeFileSync(`packages/${env}/${env}-extension-gitlab-runners/manifest.json`, manifestWithVersion)
+}
 
-// Prepare package
-targz.compress(
-  {
-    src: 'package',
-    dest: 'extension.tar.gz'
-  },
-  err => {
-    if (err) {
-      console.error(err)
+function zipFiles (env) {
+  // Prepare zip package
+  targz.compress(
+    {
+      src: `packages/${env}`,
+      dest: `extension-${env}.tar.gz`
+    },
+    err => {
+      if (err) {
+        console.error(err)
+      }
+      fs.removeSync(`packages/${env}`)
     }
-    fs.removeSync('package')
-  }
-)
+  )
+}
+
+fs.emptyDirSync('packages')
+;['chrome', 'firefox'].forEach(browser => {
+  copyFiles(browser)
+  zipFiles(browser)
+})
